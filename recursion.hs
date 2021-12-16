36e8e0ec8c55ec5ad6f912c68f22e93b12f492e1
Abdulsametileri <abdulsamet.ileri@ceng.deu.edu.tr>
2021-12-16 08:09:21 +0300

    Add recursion examples more

--- recursion.hs
+++ recursion.hs
@@ -1,3 +1,33 @@
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+        | n <= 0 = []
+        | otherwise = x:replicate' (n-1) x
+
+take' :: (Ord t, Num t) => t -> [a] -> [a]
+take' n _
+        | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1)xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+      let smallerSorted = quicksort [a | a <- xs, a <= x]
+          biggerSorted = quicksort [a | a <- xs, a > x]
+      in  smallerSorted ++ [x] ++ biggerSorted
+
 {- 
 1 Identify the end goal(s).
 2 Determine what happens when a goal is reached.
